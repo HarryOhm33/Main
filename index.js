49ef11c9a3e05a5fc9251ac3fd7e5cdbdc8efc10
Hari Om <hari333333om@gmail.com>
2023-09-15 00:45:45 +0530

    Added Some Pages

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const jammu = require("./jammu");
 const kashmir = require("./kashmir");
 const districts = jammu.concat(kashmir);
 
+// app.use(express.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 app.use(express.static(path.join(__dirname, "public")));
@@ -23,12 +25,22 @@ app.get("/contact", (req, res) => {
   res.render("contact.ejs");
 });
 
+app.get("/Products&Districts", (req, res) => {
+  res.render("p&d.ejs", { districts, jammu, kashmir });
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/login/forget_pass", (req, res) => {
+  res.render("forget.ejs");
+});
+
 app.get("/:district", (req, res) => {
   let { district } = req.params;
   let districtInfo = districts.find((d) => district === d.name);
-  res.send(
-    `This page belongs to District: ${districtInfo.name} Division: ${districtInfo.division}`
-  );
+  res.render("district.ejs", { districtInfo });
 });
 
 app.listen(port, () => {
